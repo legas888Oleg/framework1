71d2cfaae409f1d891f3effee688ab1adb65d743
Oleg Kolpakov <00legas00@gmail.com>
2020-01-06 21:31:10 +0300

    Пилю несколько методов для получения данных из базы

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,6 +11,12 @@ class MainController extends AppController {
     public function indexAction() {
         $model = new Main;
         $posts = $model->findAll();
+//        $postOne = $model->findOne('Пост 5', 'title');
+//        $data = $model->findBySQL("SELECT * FROM posts ORDER BY id DESC LIMIT 2");
+//        $data = $model->findBySQL("SELECT * FROM {$model->table} WHERE title LIKE ?", ['%2%']);
+//        $data = $model->findLike('2', 'title');
+
+//        debug($data);
 
         $title = 'PAGE TITLE';
         $this->set(compact('title', 'posts'));
--- vendor/core/base/Model.php
+++ vendor/core/base/Model.php
@@ -7,6 +7,7 @@ use vendor\core\Db;
 abstract class Model {
     protected $pdo;
     protected $table;
+    protected $primaryKey = 'id';
 
     public function __construct() {
         $this->pdo = Db::instance();
@@ -19,5 +20,21 @@ abstract class Model {
     public function findAll(){
         $sql = "SELECT * FROM {$this->table}";
         return $this->pdo->query($sql);
+    }
+
+    public function findOne($id, $field = ''){
+        $field = $field ?: $this->primaryKey;
+        $sql = "SELECT * FROM {$this->table} WHERE $field = ? LIMIT 1";
+        return $this->pdo->query($sql, [$id]);
+    }
+
+    public function findBySQL($sql, $params = []){
+        return $this->pdo->query($sql, $params);
+    }
+
+    public function findLike($str, $field, $table = ''){
+        $table = $table ?: $this->table;
+        $sql = "SELECT * FROM $table WHERE $field LIKE ?";
+        return $this->pdo->query($sql, ['%' .$str. '%']);
     }
 }
